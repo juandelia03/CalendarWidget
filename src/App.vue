12dbfc919142c2064429e5cf4f25ec642fad0d8e
juandelia03 <juanchif5017@gmail.com>
2021-08-17 09:14:34 -0300

    calendar view finished

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link>  -->
+    <!-- <router-link to="/about">About</router-link> -->
+    <div class="view">
+      <CalendarComponent @dateSelected="newDate" v-if="false" />
+    </div>
   </div>
   <router-view />
 </template>
 
+<script>
+/* eslint-disable */
+import CalendarComponent from "./components/Calendar.vue";
+export default {
+  name: "App",
+  components: {
+    CalendarComponent,
+  },
+  data() {
+    return {
+      fecha: "",
+    };
+  },
+  methods: {
+    newDate(e) {
+      this.fecha = e;
+    },
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+* {
+  font-family: "Roboto", sans-serif;
+  margin: 0;
+  padding: 0;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.view {
+  margin: 100px;
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  height: 100%;
 }
 </style>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="calendar">
+    <h1 class="title">Select a date</h1>
+    <div style="width: 100%">
+      <DatePicker
+        v-model="date"
+        @click="test"
+        color="green"
+        :is-expanded="stretch"
+        :rows="2"
+        style="margin-top: 40px"
+      />
+      <p class="succes">This date is available!</p>
+    </div>
+    <div class="buttons">
+      <button class="back">Back</button>
+      <button class="button">Check</button>
+      <button class="button">Next</button>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import { Calendar, DatePicker } from "v-calendar";
+export default {
+  name: "CalendarComponent",
+  components: {
+    Calendar,
+    DatePicker,
+  },
+  data() {
+    return {
+      date: new Date(),
+      stretch: true,
+    };
+  },
+  methods: {
+    test() {
+      this.$emit("dateSelected", this.date);
+    },
+  },
+  created() {
+    if (window.innerWidth < 560) {
+      this.stretch = false;
+    }
+  },
+};
+</script>
+
+<style scoped>
+.calendar {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+.buttons {
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 10px;
+}
+.button {
+  height: 30px;
+  width: 100px;
+  background-color: white;
+  border: none;
+  border: solid 1px black;
+  border-radius: 25px;
+  cursor: pointer;
+}
+.button:hover {
+  background: whitesmoke;
+}
+.error {
+  position: relative;
+  bottom: 30px;
+  left: 90%;
+  color: red;
+}
+.succes {
+  position: relative;
+  bottom: 30px;
+  left: 90%;
+  color: #67d82e;
+}
+.backButton {
+  display: flex;
+  width: 100%;
+}
+.back {
+  height: 30px;
+  width: 100px;
+  background-color: white;
+  border: none;
+  border: solid 1px black;
+  border-radius: 25px;
+  cursor: pointer;
+}
+.back:hover {
+  background: whitesmoke;
+}
+</style>
